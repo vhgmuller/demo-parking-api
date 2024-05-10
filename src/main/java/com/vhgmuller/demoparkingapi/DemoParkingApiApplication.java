171b9dd04f9d18685bb41c23ca5656a76d09de79
vhgmuller <vhgmuller@gmail.com>
2024-05-09 21:57:59 -0300

    Initial structure and configuration

---
+++ src/main/java/com/vhgmuller/demoparkingapi/DemoParkingApiApplication.java
@@ -0,0 +1,13 @@
+package com.vhgmuller.demoparkingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoParkingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoParkingApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/vhgmuller/demoparkingapi/config/SpringTimezoneConfig.java
@@ -0,0 +1,16 @@
+package com.vhgmuller.demoparkingapi.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
+
+@Configuration
+public class SpringTimezoneConfig {
+
+    @PostConstruct
+    public void timezoneConfig() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
+
+}
